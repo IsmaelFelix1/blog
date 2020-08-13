b642992b451f888845da554664dcd7d37de04a05
Ismael Felix <ismael.duarte017@gmail.com>
2020-08-13 11:38:08 -0300

    blog

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField('Titulo', max_length=255)
+    photo = models.ImageField('Imagem')
+    summary = RichTextField('Resumo')
+    content = RichTextUploadingField('Conteudo')
+    author = models.CharField('Autor', max_length=50)
+    created_at = models.DateField('Criado', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return self.title
+
